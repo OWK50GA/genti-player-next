508dc775a714df15f9de1445d89a1da644f8580f
OWK50GA <okoriewilfrid.20201206203@futo.edu.ng>
2025-04-22 05:46:19 +0100

    changes for the metadata

--- src/app/(unprotected)/title/[id]/page.jsx
+++ src/app/(unprotected)/title/[id]/page.jsx
@@ -1,36 +1,49 @@
 import { unauthorizedAxios } from "@/client/axios";
 import ENDPOINTS from "@/client/endpoints";
 import PreviewComponent from "@/page-components/Preview/PreviewComponent";
-import { string } from "yup";
+
+export const dynamic = "force-dynamic"
+export const revalidate = 0
 
 export async function generateMetadata({ params }, parent){
     const { id } = await params;
 
-    const res = await unauthorizedAxios.get(`${ENDPOINTS.GET_SHARE_DETAILS}/${id}`);
-    const titleData = res.data;
+    // const res = await unauthorizedAxios.get(`${ENDPOINTS.GET_SHARE_DETAILS}/${id}`);
+    // const titleData = res.data;
+
+    const res = await fetch(`${ENDPOINTS.GET_SHARE_DETAILS}/${id}`,
+        { cache: 'no-store' }
+    );
+    if (!res.ok) throw new Error("Failed to fetch share details")
+    
+    const titleData = await res.json();
+    const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
+    const imageUrl = new URL(titleData.image, siteUrl).href;
 
     if (!titleData) return
 
     return {
-        title: `${titleData?.title} | Genti Audio` || 'Genti Audio',
+        metadataBase: new URL(siteUrl),
+        title: `${titleData?.title} | Genti Audio`,
         description: `Listen to ${titleData?.title} by ${titleData?.author} on Genti Audio. Stream African stories, audio dramas, and podcasts.`,
         openGraph: {
-            title: `${titleData.title} | Genti Audio` || 'Genti Audio',
+            title: `${titleData.title} | Genti Audio`,
             description: `Listen to ${titleData?.title} by ${titleData?.author} on Genti Audio. Stream African stories, audio dramas, and podcasts.`,
+            url: `${siteUrl}/preview/${id}`,
             images: [
                 {
-                    url: `${titleData?.image}`,
+                    url: imageUrl,
                     width: 1200,
                     height: 630,
-                    alt: `${titleData?.title} | Genti Audio` || 'Genti Audio'
+                    alt: `${titleData?.title} | Genti Audio`
                 }
             ]
         },
         twitter: {
             card: `summary_large_image`,
-            title: `${titleData.title} | Genti Audio` || 'Genti Audio',
+            title: `${titleData.title} | Genti Audio`,
             description: `Listen to ${titleData?.title} by ${titleData?.author} on Genti Audio. Stream African stories, audio dramas, and podcasts.`,
-            images: [titleData?.image]
+            images: [imageUrl]
         }
     }
 }
